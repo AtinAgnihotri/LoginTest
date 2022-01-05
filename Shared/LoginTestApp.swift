c4d050642bb67dd8afc6abc2d88bcc078f296b05
Atin Agnihotri <atinagnihotri@gmail.com>
2022-01-05 11:28:35 +0530

    Initial Commit

---
+++ Shared/LoginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTestApp.swift
+//  Shared
+//
+//  Created by Atin Aganihotri on 05/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
